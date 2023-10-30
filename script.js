c3242d0965f55519428b84751917381d85ed9b7b
uendeloliveira <uos@outlook.com.br>
2023-10-30 16:09:02 -0300

    update

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Passar carrossel
 const carrossel = document.querySelector(".carrossel");
 const points = document.querySelectorAll(".points > div");
 const beforeButton = carrossel.querySelector(".before");
@@ -16,61 +17,24 @@ beforeButton.addEventListener("click", () => {
     selectedIndex = (selectedIndex - 1 + points.length) % points.length;
     points[selectedIndex].classList.add("selected");
 });
+// Passar conteúdo carrossel 
+for (let i = 2; i <= 6; i++) {
+    $(`.slid-t${i}, .slide-b${i}, .slide-bb${i}`).hide();
+}
+  
+$('.after, .before').click(() => {
+const points = ['.point1', '.point2', '.point3', '.point4', '.point5', '.point6'];
+const elements = ['.slid-t', '.slide-b', '.slide-bb'];
 
-
-$('.slid-t2, .slide-b2, .slide-bb2').hide();
-$('.slid-t3, .slide-b3, .slide-bb3').hide();
-$('.slid-t4, .slide-b4, .slide-bb4').hide();
-$('.slid-t5, .slide-b5, .slide-bb5').hide();
-$('.slid-t6, .slide-b6, .slide-bb6').hide();
-
-$('.after, .before').click(()=>{
-    if($('.point1').hasClass('selected')){
-        $('.slid-t1, .slide-b1, .slide-bb1').show();
-        $('.slid-t2, .slide-b2, .slide-bb2').hide();
-        $('.slid-t3, .slide-b3, .slide-bb3').hide();
-        $('.slid-t4, .slide-b4, .slide-bb4').hide();
-        $('.slid-t5, .slide-b5, .slide-bb5').hide();
-        $('.slid-t6, .slide-b6, .slide-bb6').hide();
+points.forEach((point, index) => {
+    if ($(point).hasClass('selected')) {
+        elements.forEach(elementClass => {
+            $(`${elementClass}${index + 1}`).show();
+        });
+    } else {
+        elements.forEach(elementClass => {
+            $(`${elementClass}${index + 1}`).hide();
+        });
     }
-    if($('.point2').hasClass('selected')){
-        $('.slid-t1, .slide-b1, .slide-bb1').hide();
-        $('.slid-t2, .slide-b2, .slide-bb2').show();
-        $('.slid-t3, .slide-b3, .slide-bb3').hide();
-        $('.slid-t4, .slide-b4, .slide-bb4').hide();
-        $('.slid-t5, .slide-b5, .slide-bb5').hide();
-        $('.slid-t6, .slide-b6, .slide-bb6').hide();
-    }
-    if($('.point3').hasClass('selected')){
-        $('.slid-t1, .slide-b1, .slide-bb1').hide();
-        $('.slid-t2, .slide-b2, .slide-bb2').hide();
-        $('.slid-t3, .slide-b3, .slide-bb3').show();
-        $('.slid-t4, .slide-b4, .slide-bb4').hide();
-        $('.slid-t5, .slide-b5, .slide-bb5').hide();
-        $('.slid-t6, .slide-b6, .slide-bb6').hide();
-    }
-    if($('.point4').hasClass('selected')){
-        $('.slid-t1, .slide-b1, .slide-bb1').hide();
-        $('.slid-t2, .slide-b2, .slide-bb2').hide();
-        $('.slid-t3, .slide-b3, .slide-bb3').hide();
-        $('.slid-t4, .slide-b4, .slide-bb4').show();
-        $('.slid-t5, .slide-b5, .slide-bb5').hide();
-        $('.slid-t6, .slide-b6, .slide-bb6').hide();
-    }
-    if($('.point5').hasClass('selected')){
-        $('.slid-t1, .slide-b1, .slide-bb1').hide();
-        $('.slid-t2, .slide-b2, .slide-bb2').hide();
-        $('.slid-t3, .slide-b3, .slide-bb3').hide();
-        $('.slid-t4, .slide-b4, .slide-bb4').hide();
-        $('.slid-t5, .slide-b5, .slide-bb5').show();
-        $('.slid-t6, .slide-b6, .slide-bb6').hide();
-    }
-    if($('.point6').hasClass('selected')){
-        $('.slid-t1, .slide-b1, .slide-bb1').hide();
-        $('.slid-t2, .slide-b2, .slide-bb2').hide();
-        $('.slid-t3, .slide-b3, .slide-bb3').hide();
-        $('.slid-t4, .slide-b4, .slide-bb4').hide();
-        $('.slid-t5, .slide-b5, .slide-bb5').hide();
-        $('.slid-t6, .slide-b6, .slide-bb6').show();
-    }
-})
+});
+});
